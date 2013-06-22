0d9b40c1cb3f348ef5ae40961226ab6beda17a15
vinit kumar <vinit.kumar@changer.nl>
2013-06-22 22:13:34 +0530

    adding twitter widget in template

--- cmsplugin_twitter/cms_plugins.py
+++ cmsplugin_twitter/cms_plugins.py
@@ -2,17 +2,18 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.utils.translation import ugettext_lazy as _
 
-from models import twitter
+from models import Twitter
+
 
 class TwitterPlugin(CMSPluginBase):
-    model = Fb
+    model = Twitter
     name = _("Django CMS Twitter")
     render_template = "cms_plugins/twitter.html"
 
     def render(self, context, instance, placeholder):
         context.update({
-          'object': instance,
-          })
+            'object': instance,
+        })
         return context
 
 plugin_pool.register_plugin(TwitterPlugin)
