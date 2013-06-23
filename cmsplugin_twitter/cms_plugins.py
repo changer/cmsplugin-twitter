7ae9ef5b5034503d758f8c168353e75e9526a6e4
vinit kumar <vinit.kumar@changer.nl>
2013-06-23 15:00:20 +0530

    fixed the typo

--- cmsplugin_twitter/cms_plugins.py
+++ cmsplugin_twitter/cms_plugins.py
@@ -1,4 +1,4 @@
-from cmss.plugin_base import CMSPluginBase
+from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.utils.translation import ugettext_lazy as _
 
