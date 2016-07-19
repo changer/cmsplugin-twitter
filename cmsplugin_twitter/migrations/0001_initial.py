a0ae9fad1d8b656b14f5af5193bfe009e9bac831
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2016-07-19 13:21:43 +0530

    upgrade migrations

--- cmsplugin_twitter/migrations/0001_initial.py
+++ cmsplugin_twitter/migrations/0001_initial.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations, models
+from django.db import models, migrations
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cms', '0013_urlconfrevision'),
+        ('cms', '0014_auto_20160404_1908'),
     ]
 
     operations = [
